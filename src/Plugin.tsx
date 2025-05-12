49454b92166a207efeeb8cb02e115e778309c443
Matias Arriola <matias@eyeseetea.com>
2025-05-12 14:17:54 -0300

    fix: apply error workaround to future date validation

--- src/Plugin.tsx
+++ src/Plugin.tsx
@@ -6,48 +6,46 @@ import i18n from "@dhis2/d2-i18n";
 import { calculateAge } from "./utils/calculateAge";
 
 const PluginInner = (propsFromParent: IDataEntryPluginProps) => {
+  const setError = (value: string, error: string) => {
+    // HACK: set the value twice with different values to trigger the error message
+    // If this is not done, the error message will be show only after blurring the next field
+    // tested with Capture 101.32.5
+    propsFromParent.setFieldValue({
+      fieldId: PluginFields.dateOfBirth,
+      value: value + " ",
+      options: {
+        valid: false,
+        touched: true,
+        error,
+      },
+    });
+    propsFromParent.setFieldValue({
+      fieldId: PluginFields.dateOfBirth,
+      value: value,
+      options: {
+        valid: false,
+        touched: true,
+        error,
+      },
+    });
+  };
+
   React.useEffect(() => {
     const inputDateOfBirth = propsFromParent.values.dateOfBirth;
     const formattedDateOfBirth = formatDate(inputDateOfBirth);
     if (inputDateOfBirth) {
       if (!formattedDateOfBirth) {
-        // HACK: set the value twice with different values to trigger the error message
-        // If this is not done, the error message will be show only after blurring the next field
-        // tested with Capture 100.32.5
-        propsFromParent.setFieldValue({
-          fieldId: PluginFields.dateOfBirth,
-          value: inputDateOfBirth + " ",
-          options: {
-            valid: false,
-            touched: true,
-            error: i18n.t(
-              "Invalid date format, please use YYYY-MM-DD or YYYYMMDD"
-            ),
-          },
-        });
-        propsFromParent.setFieldValue({
-          fieldId: PluginFields.dateOfBirth,
-          value: inputDateOfBirth,
-          options: {
-            valid: false,
-            touched: true,
-            error: i18n.t(
-              "Invalid date format, please use YYYY-MM-DD or YYYYMMDD"
-            ),
-          },
-        });
+        setError(
+          inputDateOfBirth,
+          i18n.t("Invalid date format, please use YYYY-MM-DD or YYYYMMDD")
+        );
       } else if (
         new Date(formattedDateOfBirth).getTime() > new Date().getTime()
       ) {
-        propsFromParent.setFieldValue({
-          fieldId: PluginFields.dateOfBirth,
-          value: inputDateOfBirth,
-          options: {
-            valid: false,
-            touched: true,
-            error: i18n.t("Date of Birth cannot be in the future"),
-          },
-        });
+        setError(
+          inputDateOfBirth,
+          i18n.t("Date of Birth cannot be in the future")
+        );
       } else {
         propsFromParent.setFieldValue({
           fieldId: PluginFields.dateOfBirth,
